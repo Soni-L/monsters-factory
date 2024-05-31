7e6df54fda8491ed276d73516d040a72260faa95
soni <acdsoni@gmail.com>
2024-05-31 14:24:26 +0200

    fetch monsters with pagination

--- packages/app/src/routes/DisplayMonsters.tsx
+++ packages/app/src/routes/DisplayMonsters.tsx
@@ -1,10 +1,72 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import TablePagination from "@mui/material/TablePagination";
 const BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL;
 
-export default function DisplayMonsters() {
-  useEffect(() => {
-    fetch(BACKEND_URL);
-  }, []);
+interface Monster {
+  _id: string;
+  name: string;
+  type: object;
+}
 
-  return <div>DisplayMonsters</div>;
+interface MonstersResponse {
+  total: number;
+  page: number;
+  limit: number;
+  monsters: Monster[];
+}
+
+export default function DisplayMonsters() {
+  const [monsters, setMonsters] = useState([]);
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+  const [totalElements, setTotalElements] = useState(1);
+
+  useEffect(() => {
+    const fetchMonsters = async () => {
+      try {
+        const response = await fetch(
+          `${BACKEND_URL}?page=${page}&limit=${limit}`
+        );
+        const data: MonstersResponse = await response.json();
+
+        setMonsters(data.monsters);
+        setTotalElements(data.total);
+      } catch (error) {
+        console.error("Error fetching monsters:", error);
+      }
+    };
+
+    fetchMonsters();
+  }, [page, limit]);
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage + 1);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setLimit(parseInt(event.target.value, 10));
+    setPage(1);
+  };
+
+  return (
+    <div style={{ padding: "10px" }}>
+      {monsters.map((monster: Monster) => (
+        <div key={monster._id}>{monster.name}</div>
+      ))}
+
+      <TablePagination
+        component="div"
+        count={totalElements}
+        page={page - 1}
+        onPageChange={handleChangePage}
+        rowsPerPage={limit}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </div>
+  );
 }
--- packages/backend/src/routes/monster.ts
+++ packages/backend/src/routes/monster.ts
@@ -4,8 +4,24 @@ import Monster from "../models/monster";
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
-  //   const users = await User.find();
- res.json({ user : 'usr'});
+  try {
+    // Get page and limit from query parameters, set defaults if not provided
+    const page = parseInt(req.query.page as string) || 1;
+    const limit = parseInt(req.query.limit as string) || 10;
+    const skip = (page - 1) * limit;
+
+    const monsters = await Monster.find({}).skip(skip).limit(limit);
+    const total = await Monster.countDocuments({});
+
+    res.json({
+      total,
+      page,
+      limit,
+      monsters,
+    });
+  } catch (err) {
+    res.status(500).json({ error: "could not retreive monsters" });
+  }
 });
 
 router.post("/", async (req: Request, res: Response) => {
