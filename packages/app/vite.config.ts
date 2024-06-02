8e05f24f93ec5cf745067291c2d71669eac57656
soni <acdsoni@gmail.com>
2024-06-02 02:27:53 +0200

    sucessful docker compose run

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -5,11 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true, // Here
-    strictPort: true,
-    port: 8080,
+    host: true,
   },
 });
